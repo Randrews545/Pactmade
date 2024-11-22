8e496cedb664c8e8524f490b824784bfc47ba61c
Ryan Andrews <randrews545@gmail.com>
2024-11-21 20:31:22 -0500

    Completed videos 4-7 of boardgame tutorial.

--- tutorial/main.gd
+++ tutorial/main.gd
@@ -2,16 +2,21 @@ extends Node2D
 
 @onready var pink_piece : Sprite2D = $PinkPiece
 @export var game_spaces : Array[Node]
-var place : int = 0
+var place : int = 1
 var number_of_spaces : int
+@onready var dice := $Dice
+@onready var timer := $Timer
 
 func _ready() -> void:
 	number_of_spaces = game_spaces.size()
+	print(number_of_spaces)
 
-func _unhandled_input(event: InputEvent) -> void:
-	if Input.is_action_just_pressed("ui_click") and place < number_of_spaces:
+func _on_dice_dice_has_rolled(roll: Variant) -> void:
+	print(roll)
+	while roll != 0:
 		var tween = create_tween()
 		tween.tween_property(pink_piece, "position", game_spaces[place].position, 1)
+		timer.start()
+		await timer.timeout
 		place += 1
-	elif place >= number_of_spaces:
-		print("Place is out of bounds")
+		roll -= 1
