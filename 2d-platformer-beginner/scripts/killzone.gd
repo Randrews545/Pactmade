355609d719052f01aacaa3fc6d84ed311e82c419
Ryan Andrews <randrews545@gmail.com>
2024-12-14 17:24:16 -0500

    Work in progress on erasing ice blocks with fire.

--- 2d-platformer-beginner/scripts/killzone.gd
+++ 2d-platformer-beginner/scripts/killzone.gd
@@ -1,15 +1,28 @@
 extends Area2D
+
 const SPEED = 10
 
 var direction = -1
 
 @onready var timer: Timer = $Timer
+@onready var tiles: TileMapLayer = $"../TileMap/Mid"
+@onready var tilemap: TileMap = $"../TileMap"
 
 func _on_body_entered(body: Node2D) -> void:
-	print("You died!")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+	match body.name:
+		"Player":
+			print("You died!")
+			Engine.time_scale = 0.5
+			body.get_node("CollisionShape2D").queue_free()
+			timer.start()
+		"Mid":
+			var tile = tilemap.local_to_map(tilemap.to_local(to_global(body.position)))
+			tiles.erase_cell(tile)
+			#var tilecords = body.get_cell_atlas_coords(tile)
+			#var tileid = body.get_cell_source_id(tilecords)
+			print(tile)
+			#print(tilecords)
+			#print(tileid)
 
 
 func _on_timer_timeout() -> void:
